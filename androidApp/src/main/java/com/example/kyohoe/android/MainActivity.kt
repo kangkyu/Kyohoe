622daabf2f23cedc0aeb66afe1c20f986fae3a73
Kang-Kyu Lee <kangkyu1111@gmail.com>
2023-06-09 22:28:22 -0700

    First, play a youtube video on the app (webview)
    Use your youtube api key in place of "YOUR_API_KEY"

--- androidApp/src/main/java/com/example/kyohoe/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/kyohoe/android/MainActivity.kt
@@ -4,37 +4,38 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kyohoe.Greeting
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.rememberWebViewState
+import com.google.accompanist.web.rememberWebViewStateWithHTMLData
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    GreetingView(Greeting().greet())
-                }
-            }
+            ExampleYouTube()
         }
     }
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun ExampleYouTube() {
+//    val webViewState = rememberWebViewState(url = "https://www.kangkyu.com")
+    val webViewState = rememberWebViewStateWithHTMLData(data = getHTML("Crgj2rwVZQk"))
+    WebView(
+        state = webViewState,
+        modifier = Modifier.fillMaxSize(),
+        onCreated = { webView ->
+            webView.settings.javaScriptEnabled = true
+        }
+    )
 }
 
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
-    }
+fun getHTML(ytId: String): String {
+    return """
+<iframe class="youtube-player" style="border: 0; width: 100%; height: 95%; padding:0px; margin:0px" id="ytplayer" type="text/html" src="https://www.youtube.com/embed/${ytId}?fs=0" frameborder="0">
+</iframe>
+"""
 }
